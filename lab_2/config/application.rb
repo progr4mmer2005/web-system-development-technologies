97845a52a49c23713fa486cc24d680a141ebf357
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-20 13:01:37 +0300

    feat(lab-2): initialize Rails application

---
+++ lab_2/config/application.rb
@@ -0,0 +1,19 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Lab2
+  class Application < Rails::Application
+    config.load_defaults 8.1
+
+    config.autoload_lib(ignore: %w[assets tasks])
+
+  end
+end
